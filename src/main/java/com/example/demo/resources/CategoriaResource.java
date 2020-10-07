c56d5d8e63e7fa1ea48cd051e4a677bf7bc736ff
Silvio Limeira <sicemal@gmail.com>
2020-10-07 16:54:44 -0300

    ResponseEntith with type Categoria

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -28,7 +28,7 @@ public class CategoriaResource {
 	}
 
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+	public ResponseEntity<Categoria> insert(@RequestBody Categoria obj) {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -36,7 +36,7 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) 
+	public ResponseEntity<Categoria> update(@RequestBody Categoria obj, @PathVariable Integer id) 
 	{
 		obj.setId(id);
 		obj = service.update(obj);
