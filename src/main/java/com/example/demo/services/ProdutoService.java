ab9d3478ddaab26f8494ac193bfbe1d2ec8f27a4
Silvio Limeira <sicemal@gmail.com>
2020-10-07 16:50:58 -0300

    add PUT method in Categoria endpoint

--- src/main/java/com/example/demo/services/ProdutoService.java
+++ src/main/java/com/example/demo/services/ProdutoService.java
@@ -15,7 +15,7 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 
-	public Produto buscar(Integer id) {
+	public Produto find(Integer id) {
 		Optional<Produto> obj = produtoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				Produto.class.getName() + " - Não encontrado! Id: " + id + " Tipo: " + Produto.class.getName()));
