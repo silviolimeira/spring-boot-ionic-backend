29755d605f614c12fa9aafd51ab120276dae01ff
Silvio Limeira <sicemal@gmail.com>
2020-10-07 10:37:13 -0300

    add Cliente resource and service

---
+++ src/main/java/com/example/demo/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.example.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Cliente;
+import com.example.demo.repositories.ClienteRepository;
+import com.example.demo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				Cliente.class.getName() + " - Não encontrado! Id: " + id + " Tipo: " + Cliente.class.getName()));
+	}
+}
