454295cf613bcd11c99577e796065835edb42bf6
Silvio Limeira <sicemal@gmail.com>
2020-10-07 16:37:11 -0300

    add POST to insert a new Categoria

--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -20,4 +20,9 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				Categoria.class.getName() + " - Não encontrado! Id: " + id + " Tipo: " + Categoria.class.getName()));
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
 }
