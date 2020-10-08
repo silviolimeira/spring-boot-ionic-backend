911f83a98d59aebf8f4a0999991840bf2934705c
Silvio Limeira <sicemal@gmail.com>
2020-10-08 08:22:07 -0300

    add sintatic validation Categoria

--- src/main/java/com/example/demo/dto/CategoriaDTO.java
+++ src/main/java/com/example/demo/dto/CategoriaDTO.java
@@ -2,12 +2,26 @@ package com.example.demo.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.example.demo.domain.Categoria;
 
+
 public class CategoriaDTO implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	// Entidade Categoria
+	//
+	// . Validações sintáticas
+	//   . Nome não pode ser vazio
+	//   . Nome deve conter entre 5 e 80 caracteres
+	@NotEmpty
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {}
--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.dto.CategoriaDTO;
 import com.example.demo.repositories.CategoriaRepository;
 import com.example.demo.services.exceptions.DataIntegrityException;
 import com.example.demo.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
